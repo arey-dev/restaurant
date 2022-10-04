70dd7bf8e341aaacb053deda988f33322aa6eab5
R.A Borja <raborja.dev@gmail.com>
2022-10-04 19:05:26 +0800

    add about section dom elements

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles/reset.css';
 import './styles/base.css';
 import './styles/index.css';
+import about_img from './images/about_img.jpg';
 
 const header = () => {
   // create header elements
@@ -35,6 +36,63 @@ const header = () => {
   header.append(header_cont);
 
   return header;
+};
+
+const home = () => {
+  const elems = [];
+
+  // create home elements
+  const about = document.createElement('section');
+  const about_cont = document.createElement('div');
+  const about_h2 = document.createElement('h2');
+  const about_p = document.createElement('p');
+  const about_fig = document.createElement('figure');
+  const about_fig_img = document.createElement('img');
+  const about_fig_cap = document.createElement('figcaption');
+
+  // const staff = document.createElement('section');
+  // const staff_cont = document.createElement('div');
+  // const staff_h2 = document.createElement('h2');
+  // const staff_art1 = document.createElement('article');
+  // const staff_art2 = document.createElement('article');
+
+  // add classes and id
+  about.setAttribute('id', 'about');
+  about_cont.className = 'container container--about';  
+
+  // staff_cont.classList.add('continaer');
+  // staff_art1.classList.add('staff_card');
+  // staff_art2.classList.add('staff_card');
+
+  // add contents to elements
+  about_h2.textContent = 'About';
+  about_p.textContent = 'Elise Cravings is a restaurant that will satisfy your cravings. We have a wide variety of food choices for people with different tastes. Aside from the delicious food, Elise Cravings have a nice ambience that will make you enjoy every bite!';
+  about_fig_img.src = about_img;
+  about_fig_cap.textContent = 'Elise Cravings on a typical busy day.';
+
+
+  // staff_h2.textContent = 'Staff';
+
+  // append elements
+  about_fig.append(about_fig_img);
+  about_fig.append(about_fig_cap);
+  about_cont.append(about_h2);
+  about_cont.append(about_p);
+  about_cont.append(about_fig);
+  about.append(about_cont);
+
+  // push elements to array
+  elems.push(about);
+
+  return elems;
 }
 
+const main = () => {
+  const main = document.createElement('main');
+  main.append(...home());
+
+  return main;
+};
+
 content.append(header());
+content.append(main());
