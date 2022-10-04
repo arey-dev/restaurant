5e302072947d6ffbeb180fc595282faeeded4406
R.A Borja <raborja.dev@gmail.com>
2022-10-04 15:40:35 +0800

    add dom elements for header

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
+import './styles/reset.css';
+import './styles/base.css';
 import './styles/index.css';
 
-const text = document.createElement('p');
-text.textContent = 'Hello World!';
+const header = () => {
+  // create header elements
+  const header = document.createElement('header');
+  const header_cont = document.createElement('div');
+  const header_h1 = document.createElement('h1');
+  const header_btn_cont = document.createElement('div');
+  const header_btn1 = document.createElement('button');
+  const header_btn2 = document.createElement('button');
+  const header_btn3 = document.createElement('button');
 
-content.append(text);
+  // add class to header elements
+  header_cont.classList.add('container');
+  header_cont.classList.add('grid');
+  header_btn_cont.classList.add('btn-container');
+
+  // header contents
+  header_h1.textContent = "Elise's Stomach";
+  header_btn1.textContent = 'Home';
+  header_btn2.textContent = 'Menu';
+  header_btn3.textContent = 'Contact';
+
+  // append elements
+  header_btn_cont.append(header_btn1);
+  header_btn_cont.append(header_btn2);
+  header_btn_cont.append(header_btn3);
+  header_cont.append(header_h1);
+  header_cont.append(header_btn_cont);
+  header.append(header_cont);
+
+  return header;
+}
+
+content.append(header());
