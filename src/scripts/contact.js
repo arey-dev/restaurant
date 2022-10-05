4907afab322f60a37b7dc3ad7d78b14f9fc1f5fd
R.A Borja <raborja.dev@gmail.com>
2022-10-05 14:19:01 +0800

    add contact func

---
+++ src/scripts/contact.js
@@ -0,0 +1,23 @@
+
+const contacts = () => {
+  const contacts = document.createElement('section');
+  contact.setAttribute('id', 'contacts')
+
+  // container
+  const contacts_cont = document.createElement('div');
+  contacts_cont.className = 'container container--contacts';
+
+  const contacts_h2 = document.createElement('h2');
+  contacts_h2.textContent = 'Contacts';
+
+  const contacts_address = document.createElement('address');
+  contacts_address.innerHTML = '<span>Contact Us</span><a href="#">(311) 555-2368</a><span>Message Us</span><a href="#">jim@rock.com</a><span>Visit Us</span><span>192 S 500 E <br/> Fork, Utah <br/> US</span>';
+
+  contacts_cont.append(contacts_h2);
+  contacts_cont.append(contacts_address);
+  contacts.append(contacts_cont);
+  
+  return contacts
+};
+
+export default contacts;
