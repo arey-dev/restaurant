f753e737b6521a65beacbb353f441e9a5d8d001e
R.A Borja <raborja.dev@gmail.com>
2022-10-05 12:25:55 +0800

    import menu func

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './styles/base.css';
 import './styles/index.css';
 import './styles/home.css';
 // import home from './scripts/home.js';
-
+import menu from './scripts/menu.js';
 
 const header = () => {
   // create header elements
@@ -43,6 +43,7 @@ const header = () => {
 const main = () => {
   const main = document.createElement('main');
   // main.append(...home());
+  main.append(menu());
 
   return main;
 };
