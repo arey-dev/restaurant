fba71665e0facde3b8fffd935419981f573af5e7
R.A Borja <raborja.dev@gmail.com>
2022-10-05 14:41:33 +0800

    import contact module

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './styles/menu.css';
 import './styles/contact.css';
 // import home from './scripts/home.js';
 // import menu from './scripts/menu.js';
+import contacts from './scripts/contact.js';
 
 const header = () => {
   // create header elements
@@ -46,6 +47,7 @@ const main = () => {
   const main = document.createElement('main');
   // main.append(...home());
   // main.append(menu());
+  main.append(contacts());
 
   return main;
 };
