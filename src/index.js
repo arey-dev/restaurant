6a716a65bdd80dceed35254c16ba7ae0507b3db8
R.A Borja <raborja.dev@gmail.com>
2022-10-05 11:17:54 +0800

    remove home function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './styles/reset.css';
 import './styles/base.css';
 import './styles/index.css';
-import about_img from './images/about_img.jpg';
+import './styles/home.css';
+// import home from './scripts/home.js';
+
 
 const header = () => {
   // create header elements
@@ -38,47 +40,9 @@ const header = () => {
   return header;
 };
 
-const home = () => {
-  const elems = [];
-
-  // create home elements
-  const about = document.createElement('section');
-  const about_cont = document.createElement('div');
-  const about_h2 = document.createElement('h2');
-  const about_p = document.createElement('p');
-  const about_fig = document.createElement('figure');
-  const about_fig_img = document.createElement('img');
-  const about_fig_cap = document.createElement('figcaption');
-
-  // add classes and id
-  about.setAttribute('id', 'about');
-  about_cont.className = 'container container--about';  
-
-  // add contents to elements
-  about_h2.textContent = 'About';
-  about_p.textContent = 'Elise Cravings is a restaurant that will satisfy your cravings. We have a wide variety of food choices for people with different tastes. Aside from the delicious food, Elise Cravings have a nice ambience that will make you enjoy every bite!';
-  about_fig_img.src = about_img;
-  about_fig_cap.textContent = 'Elise Cravings on a typical busy day.';
-
-  // append elements
-  about_fig.append(about_fig_img);
-  about_fig.append(about_fig_cap);
-  about_cont.append(about_h2);
-  about_cont.append(about_p);
-  about_cont.append(about_fig);
-  about.append(about_cont);
-
-  // push elements to array
-  elems.push(about);
-
-  // add staff section
-
-  return elems;
-}
-
 const main = () => {
   const main = document.createElement('main');
-  main.append(...home());
+  // main.append(...home());
 
   return main;
 };
